173b877285f4486da798c3788086abbc1293a806
justin <justincj2001@gmail.com>
2023-06-11 18:37:37 +0530

    forth commit

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.get('/', (req, res) => {
     res.render('index', { habits: results });
   });
 });
+// ...
 
 // Add a new habit
 app.post('/habits', (req, res) => {
@@ -47,11 +48,18 @@ app.post('/habits', (req, res) => {
       longest_streak: 0
     };
 
-    pool.query('INSERT INTO habits SET ?', habit, (err) => {
-      if (err) throw err;
+    pool.query('INSERT INTO habits SET ?', habit, (err, results) => {
+      if (err) {
+        console.error('Error inserting new habit:', err);
+        res.status(500).send('Error inserting new habit');
+        return;
+      }
 
+      console.log('New habit added:', results.insertId);
       res.redirect('/');
     });
+  } else {
+    res.status(400).send('Name is required for adding a new habit');
   }
 });
 
@@ -63,43 +71,70 @@ app.post('/habits/:id', (req, res) => {
   if (habitId && status) {
     const dayIndex = Number(req.body.dayIndex);
 
-    let updateQuery = `UPDATE habits SET day${dayIndex}_status = ? WHERE id = ?`;
+    let updateQuery = `UPDATE habits SET day${dayIndex}_status = ?`;
 
     // Check if updating today's status
     if (dayIndex === 1) {
-      updateQuery += `, longest_streak = CASE
-        WHEN day1_status = 'None' THEN longest_streak + 1
-        WHEN day1_status = 'Done' THEN longest_streak
-        ELSE longest_streak - 1
-      END`;
+      updateQuery += `, longest_streak = 
+        CASE day1_status
+          WHEN 'None' THEN longest_streak + 1
+          WHEN 'Done' THEN longest_streak
+          ELSE longest_streak - 1
+        END`;
     }
 
-    pool.query(updateQuery, [status, habitId], (err) => {
-      if (err) throw err;
+    updateQuery += ` WHERE id = ?`;
 
+    pool.query(updateQuery, [status, habitId], (err) => {
+      if (err) {
+        console.error('Error updating habit status:', err);
+        res.status(500).send('Error updating habit status');
+        return;
+      }
+
+      console.log('Habit status updated:', habitId);
       res.redirect('/');
     });
+  } else {
+    res.status(400).send('Habit ID and status are required for updating habit status');
   }
 });
 
-// ...
 
 // Delete habit
 app.post('/habits/:id/delete', (req, res) => {
   const habitId = req.params.id;
 
   if (habitId) {
-    pool.query('DELETE FROM habits WHERE id = ?', habitId, (err) => {
-      if (err) throw err;
+    pool.query('DELETE FROM habits WHERE id = ?', habitId, (err, results) => {
+      if (err) {
+        console.error('Error deleting habit:', err);
+        res.status(500).send('Error deleting habit');
+        return;
+      }
 
-      // After deleting the habit, redirect to the home page to refresh the habit list
+      console.log('Habit deleted:', habitId);
       res.redirect('/');
     });
+  } else {
+    res.status(400).send('Habit ID is required for deleting a habit');
   }
 });
 
 // ...
 
+// Handle 404 - Page not found
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+});
+
+// Handle other errors
+app.use((err, req, res, next) => {
+  console.error('Internal Server Error:', err);
+  res.status(500).send('Internal Server Error');
+});
+
+// ...
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
